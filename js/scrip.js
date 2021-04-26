ceca5d8c92f9521091369c6178a466de2320db50
Moldobai <79972237+Moldobai@users.noreply.github.com>
2021-04-26 22:52:18 +0600

    Bag
    Bag

---
+++ js/scrip.js
@@ -0,0 +1,14 @@
+// JAvascript
+
+
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+  
